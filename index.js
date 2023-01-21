3ba12ed37500063e8be3ca66e1ffc2166e6d24b5
Bilal-Siddiqe <dev.bilalsiddique@gmail.com>
2023-01-21 14:31:43 +0500

    Maths function connected as maths.add etc and document style changed

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 let maths = {
     add: function (num1, num2) {
-        document.write(num1 + num2)
+        document.write("<center><br> Your Answer Is <br><br><br><br><b><i><h1>",num1 + num2 ,"</h1></i></b></center>")
     },
     sub: function (num1, num2) {
-        document.write(num1 - num2)
+        document.write("<center><br> Your Answer Is <br><br><br><br><b><i><h1>",num1 - num2 ,"</h1></i></b></center>")
     },
     mult: function (num1, num2) {
-        document.write(num1 * num2)
+        document.write("<center><br> Your Answer Is <br><br><br><br><b><i><h1>",num1 * num2 ,"</h1></i></b></center>")
     },
 
     divide: function (num1, num2) {
-        document.write(num1 / num2)
+        document.write("<center><br> Your Answer Is <br><br><br><br><b><i><h1>",num1 / num2 ,"</h1></i></b></center>")
     },
     reminder: function (num1, num2) {
-        document.write(num1 % num2)
+        document.write("<center><br> Your Answer Is <br><br><br><br><b><i><h1>",num1 % num2 ,"</h1></i></b></center>")
     }
 }
